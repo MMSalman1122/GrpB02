a96998e4c0362688d01d9aa26c905a8e050897d9
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-05-30 14:10:46 +0500

    Section 2 Lecture 9 code first demo

--- CodeFirst/ConsoleApp1/Program.cs
+++ CodeFirst/ConsoleApp1/Program.cs
@@ -1,11 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public class Post
+    {
+        public int Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+    public class BlogContext: DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+    }
     class Program
     {
         static void Main(string[] args)
